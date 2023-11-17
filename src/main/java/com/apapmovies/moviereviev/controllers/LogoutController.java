56f9e1b287b04f561244ca0a8d840706f76d5688
Paweł <pa.st.a@wp.pl>
2023-11-18 00:32:08 +0100

    MovieHub -v1.6 Added logout

---
+++ src/main/java/com/apapmovies/moviereviev/controllers/LogoutController.java
@@ -0,0 +1,19 @@
+package com.apapmovies.moviereviev.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogoutController {
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/login";
+    }
+}
